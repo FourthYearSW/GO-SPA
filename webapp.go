b492b16bd89ed3044224dcd9fcda9e98f9e5dc12
Will Hogan <willhogan11@hotmail.com>
2016-11-07 21:05:21 +0000

    Working on how to post data from form, firstly using bare html post method

--- webapp.go
+++ webapp.go
@@ -7,12 +7,19 @@ package main
 import "gopkg.in/macaron.v1"
 
 func main() {
-  m := macaron.Classic()
-  m.Use(macaron.Renderer())
-  
-  // Edit accordingly....
-  m.Get("/hello", func() string {
-    return "Hello from Macaron!"
-  })
-  m.Run(8080) // Start Web Server on port 8080
+	m := macaron.Classic()
+	m.Use(macaron.Renderer())
+
+	// Basic Hello world
+	m.Get("/hello", func() string {
+		return "Hello world!"
+	})
+
+	// Using the template from template folder
+	m.Get("/helloagain", func(ctx *macaron.Context) {
+		ctx.Data["Name"] = "Person"
+		ctx.HTML(200, "hello") // 200 is the response code.
+	})
+
+	m.Run(8080) // Start Web Server on port 8080
 }
