46fa8386152cc68479b483b7e63fa9ca86cf95a6
Will Hogan <willhogan11@hotmail.com>
2016-11-18 23:54:20 +0000

    Added countdown timer to webapp.go

--- webapp.go
+++ webapp.go
@@ -1,25 +1,51 @@
-// Author: Andrej Lavrinovic & Will Hogan
-// Date: November 07th, 2016
-// Adapted from: https://go-macaron.com
+// Author: Will Hogan
+// Date: 18th Nov, 2016
+// Countdown Timer Adapted from https://play.golang.org/p/jl5VwaurB5
 
 package main
 
-import "gopkg.in/macaron.v1"
+import (
+	"flag"
+	"fmt"
+	"time"
+
+	"gopkg.in/macaron.v1"
+)
 
 func main() {
 	m := macaron.Classic()
 	m.Use(macaron.Renderer())
 
-	// Basic Hello world
-	m.Get("/hello", func() string {
-		return "Hello world!"
-	})
-
-	// Using the template from template folder
-	m.Get("/helloagain", func(ctx *macaron.Context) {
-		ctx.Data["Name"] = "Person"
+	m.Get("/name", func(ctx *macaron.Context) {
+		ctx.Data["Name"] = "Will"
 		ctx.HTML(200, "hello") // 200 is the response code.
 	})
 
-	m.Run(8080) // Start Web Server on port 8080
+	// For testing purposes, uses the 'timer.tmpl'
+	m.Get("/timer", func(ctx *macaron.Context) {
+		ctx.Data["timer"] = "Countdown in progress..."
+		countdown() //  Trying to figure out how this will work with html page
+		ctx.HTML(200, "timer")
+	})
+	m.Run(8080)
+}
+
+// Countdown Timer Adapted from https://play.golang.org/p/jl5VwaurB5
+func countdown() {
+	// Create a pointer named duration
+	// See http://golang.org/pkg/flag/
+	var duration = flag.Duration("duration", 5*time.Second, "set timer duration, for example 60s, 20m, .5h, or 1d")
+	flag.Parse()
+
+	// See http://golang.org/pkg/time/#Time.Format
+	const layout = "15:04:05"
+	t := time.Now()
+	fmt.Println(t.Format(layout))
+
+	timer := time.NewTimer(*duration)
+
+	<-timer.C
+	fmt.Println("1 Hour is up (obvious shorter test time of 5 seconds set for testing), need to reset and make new call to API...")
+	t = time.Now()
+	fmt.Println(t.Format(layout))
 }
