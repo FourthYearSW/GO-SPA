b056076e28b5a60483668e32756d0582f9407fc4
Tim Daiber <Tim Daiber>
2016-11-22 23:07:26 +0000

    testing submodules

---
+++ vendor/github.com/guardian/gocapiclient2/README.md
@@ -0,0 +1,91 @@
+# gocapiclient
+
+A Golang client library for the Guardian's Content API
+
+[![CircleCI](https://circleci.com/gh/guardian/gocapiclient.svg?style=svg)](https://circleci.com/gh/guardian/gocapiclient)
+
+---
+
+This is a work in progress, and currently only supports the `ItemQuery` and `SearchQuery` query type. 
+
+See [theguardian open platorm](http://open-platform.theguardian.com/documentation/) documentation for details of query parameters.
+
+##Example usage
+
+```go
+package main
+
+import (
+	"fmt"
+	"github.com/guardian/gocapiclient"
+	"github.com/guardian/gocapiclient/queries"
+	"log"
+	"strconv"
+)
+
+func main() {
+	client := gocapiclient.NewGuardianContentClient("https://content.guardianapis.com/", "my-api-key")
+	searchQuery(client)
+	searchQueryPaged(client)
+	itemQuery(client)
+}
+
+func searchQueryPaged(client *gocapiclient.GuardianContentClient) {
+	searchQuery := queries.NewSearchQuery()
+	searchQuery.PageOffset = int64(10)
+
+	showParam := queries.StringParam{"q", "sausages"}
+	params := []queries.Param{&showParam}
+
+	searchQuery.Params = params
+
+	iterator := client.SearchQueryIterator(searchQuery)
+
+	for response := range iterator {
+		fmt.Println("Page: " + strconv.FormatInt(int64(response.CurrentPage), 10))
+		for _, v := range response.Results {
+			fmt.Println(v.ID)
+		}
+	}
+}
+
+func searchQuery(client *gocapiclient.GuardianContentClient) {
+	searchQuery := queries.NewSearchQuery()
+
+	showParam := queries.StringParam{"q", "sausages"}
+	params := []queries.Param{&showParam}
+
+	searchQuery.Params = params
+
+	err := client.GetResponse(searchQuery)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(searchQuery.Response.Status)
+	fmt.Println(searchQuery.Response.Total)
+
+	for _, v := range searchQuery.Response.Results {
+		fmt.Println(v.ID)
+	}
+}
+
+func itemQuery(client *gocapiclient.GuardianContentClient) {
+	itemQuery := queries.NewItemQuery("technology/2016/aug/12/no-mans-sky-review-hello-games")
+
+	showParam := queries.StringParam{"show-fields", "all"}
+	params := []queries.Param{&showParam}
+
+	itemQuery.Params = params
+
+	err := client.GetResponse(itemQuery)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(itemQuery.Response.Status)
+	fmt.Println(itemQuery.Response.Content.WebTitle)
+}
+```
---
+++ vendor/github.com/guardian/gocapiclient2/gocapiclient.go
@@ -0,0 +1,119 @@
+package gocapiclient
+
+import (
+	"git.apache.org/thrift.git/lib/go/thrift"
+	"github.com/guardian/gocapiclient/queries"
+	"log"
+	"net/http"
+	"time"
+)
+
+const ClientVersion = "0.1"
+
+type GuardianContentClient struct {
+	ApiKey             string
+	UserAgent          string
+	HttpClient         *http.Client
+	TargetUrl          string
+	ThriftDeserializer *thrift.TDeserializer
+}
+
+func (contentClient GuardianContentClient) makeCapiRequest(q queries.Query) (*http.Response, error) {
+	url := q.GetUrl(contentClient.TargetUrl)
+
+	start := time.Now()
+
+	log.Println("gocapiclient: GET from " + url)
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	// Currently this client only supports Thrift
+	req.Header.Set("Accept", "application/x-thrift")
+	req.Header.Set("User-Agent", "content-api-go-client/v"+string(ClientVersion))
+
+	values := req.URL.Query()
+	values.Add("api-key", contentClient.ApiKey)
+	values.Add("format", "thrift")
+
+	req.URL.RawQuery = values.Encode()
+
+	response, err := contentClient.HttpClient.Do(req)
+
+	elapsed := time.Since(start)
+	log.Printf("gocapiclient: Response took %s", elapsed)
+
+	return response, err
+}
+
+func (contentClient GuardianContentClient) GetResponse(q queries.Query) error {
+	response, err := contentClient.makeCapiRequest(q)
+
+	if err != nil {
+		return err
+	}
+
+	err = q.Deserialize(contentClient.ThriftDeserializer, response.Body)
+
+	if err != nil {
+		log.Println("gocapiclient: Error deserializing response body: " + response.Status)
+	}
+
+	return err
+}
+
+func (contentClient GuardianContentClient) SearchQueryIterator(query *queries.SearchQuery) <-chan *queries.SearchPageResponse {
+	ch := make(chan *queries.SearchPageResponse)
+
+	go func() {
+		defer close(ch)
+
+		for {
+			err := contentClient.GetResponse(query)
+
+			if err != nil {
+				log.Println("gocapiclient: Error getting response from CAPI")
+				log.Println(err)
+				break
+			}
+
+			currentPage := int64(query.Response.CurrentPage)
+			totalPages := int64(query.Response.Pages)
+
+			pageResponse := &queries.SearchPageResponse{err, query.Response}
+
+			ch <- pageResponse
+
+			if currentPage >= totalPages {
+				break
+			}
+
+			query.NextPage()
+		}
+
+	}()
+
+	return ch
+}
+
+func NewGuardianContentClient(targetUrl string, apiKey string) *GuardianContentClient {
+	transport := thrift.NewTMemoryBufferLen(1024)
+	protocol := thrift.NewTCompactProtocolFactory().GetProtocol(transport)
+
+	deser := &thrift.TDeserializer{
+		transport,
+		protocol}
+
+	userAgent := "content-api-go-client/v" + string(ClientVersion)
+	client := GuardianContentClient{
+		ApiKey:             apiKey,
+		UserAgent:          userAgent,
+		HttpClient:         &http.Client{},
+		TargetUrl:          targetUrl,
+		ThriftDeserializer: deser,
+	}
+
+	return &client
+}
