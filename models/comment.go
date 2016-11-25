c192c78a9cb6ed476f2a161727502614f4a35201
Christy Madden <christymadden80@gmail.com>
2016-11-25 11:46:09 +0000

    Added models got articles and comments

---
+++ models/comment.go
@@ -0,0 +1,10 @@
+package models
+
+// Adapted from https://github.com/swhite24/go-rest-tutorial/blob/master/models/user.go
+type (
+	// Comment is a struct which holds details about the articles and can be marshalled into json and bson
+	Comment struct {
+		ID      string `json:"id" bson:"_id"`
+		Comment string `json:"comment" bson:"comment"`
+	}
+)
